0ee9e26d6aad271d2804c7b7f76f0b3c421e0f57
Matt Linaberry <mtlinaberry@gmail.com>
2015-04-20 21:30:21 -0400

    initial push
    Added the Detail View controller and set up the table view for the View
    Controller.

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Matthew Linaberry on 4/20/15.
+//  Copyright (c) 2015 Matthew Linaberry. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func eatItButtonPressed(sender: UIBarButtonItem) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
