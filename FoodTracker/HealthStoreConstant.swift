cd2ac289610a922c9ff1f74fb3189d01d927e0c5
Matt Linaberry <mtlinaberry@gmail.com>
2015-05-02 16:05:59 -0400

    App Complete!

---
+++ FoodTracker/HealthStoreConstant.swift
@@ -0,0 +1,14 @@
+//
+//  HealthStoreConstant.swift
+//  FoodTracker
+//
+//  Created by Matt Linaberry on 5/2/15.
+//  Copyright (c) 2015 Matthew Linaberry. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthStoreConstant {
+    let healthStore: HKHealthStore? = HKHealthStore()
+}
